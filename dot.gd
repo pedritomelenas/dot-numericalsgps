6e8b8343d9cd912fec40c6e819ea2e6d9e164c59
Pedro A. Garcia-Sanchez <pedro@ugr.es>
2018-06-05 19:49:53 +0200

    minor typo

--- dot.gd
+++ dot.gd
@@ -98,7 +98,7 @@ DeclareOperation("DotRosalesGraph",[IsInt,IsNumericalSemigroup]);
 #O DotFactorizationGraph(f)
 ##
 ## f is a set of factorizations 
-## returns the graph of factorizations associated to f: a complet graph 
+## returns the graph of factorizations associated to f: a complete graph 
 ## whose vertices are the elements of f. Edges are labelled with
 ## distances between nodes they join. Kruskal algorithm is used to 
 ## draw in red a spannin tree with minimal distances. Thus the catenary
